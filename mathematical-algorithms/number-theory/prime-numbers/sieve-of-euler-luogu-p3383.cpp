3f46c2f3c7cdf91e175277f9df8eddafd47a6156
qjzTreeZ <35768171+qjzTreeZ@users.noreply.github.com>
2018-08-13 09:51:23 +0800

    Add sieve of euler

---
+++ mathematical-algorithms/number-theory/prime-numbers/sieve-of-euler-luogu-p3383.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+inline int geti() {
+    int x, f = 0;
+    char c;
+    while (!isdigit(c = getchar()))
+        if (c == '-') f = 1;
+    for (x = c - '0'; isdigit(c = getchar()); x = x * 10 + c - '0')
+        ;
+    return f ? -x : x;
+}
+
+const int N = 10000010;
+int n, m, prime[N], pcnt;
+bool nprime[N];
+
+int main() {
+    n = geti();
+    m = geti();
+    nprime[1] = true;  // IMPORTANT: 1 is NOT a prime number
+    for (int i = 2; i <= n; ++i) {
+        if (!nprime[i]) prime[++pcnt] = i;
+        for (int j = 1; j <= pcnt; ++j) {
+            int c = i * prime[j];
+            if (c > n) break;
+            nprime[c] = true;
+            if (!(i % prime[j])) break;
+        }
+    }
+    while (m--) puts(nprime[geti()] ? "No" : "Yes");
+    return 0;
+}
