0dd87d2cc538de90c8ddeb59afba424543265219
qjzTreeZ <35768171+qjzTreeZ@users.noreply.github.com>
2018-10-30 11:32:09 +0800

    Add strongly connected components

---
+++ graph-algorithms/connectivity/strongly-connected-components-tarjan-luogu-p3387.cpp
@@ -0,0 +1,98 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+inline int geti() {
+    int x, f = 0;
+    char c;
+    while (!isdigit(c = getchar()))
+        if (c == '-') f = 1;
+    for (x = c - '0'; isdigit(c = getchar()); x = x * 10 + c - '0')
+        ;
+    return f ? -x : x;
+}
+
+template <typename T>
+void puti(T x) {
+    if (x < 0) {
+        putchar('-');
+        x = -x;
+    }
+    if (x > 9) puti(x / 10);
+    putchar(x % 10 + '0');
+}
+
+template <typename T>
+void putsp(T x) {
+    puti(x);
+    putchar(' ');
+}
+
+template <typename T>
+void putln(T x) {
+    puti(x);
+    putchar('\n');
+}
+
+const int N = 10010, M = 100010;
+int n, m, val[N], e0[N], e1[M], ee[N], dst[M], dfn[N], low[N], rep[N], stk[N], top = 0, disc = 0, f[N], ans = 0;
+bool vis[N];
+
+void tarjan(int u) {
+    if (dfn[u]) return;
+    dfn[u] = low[u] = ++disc;
+    stk[++top] = u;
+    vis[u] = true;
+    for (int e = e0[u]; e; e = e1[e]) {
+        int v = dst[e];
+        if (!dfn[v]) {
+            tarjan(v);
+            low[u] = min(low[u], low[v]);
+        } else if (vis[v])
+            low[u] = min(low[u], dfn[v]);
+    }
+    if (dfn[u] == low[u])
+        while (int p = stk[top--]) {
+            rep[p] = u;
+            vis[p] = false;
+            if (p == u) break;
+            val[u] += val[p];
+            if (e0[p]) {
+                if (ee[u]) {
+                    e1[ee[u]] = e0[p];
+                    ee[u] = ee[p];
+                } else {
+                    e0[u] = e0[p];
+                    ee[u] = ee[p];
+                }
+            }
+        }
+}
+
+int dfs(int u) {
+    u = rep[u];
+    if (f[u]) return f[u];
+    for (int e = e0[u]; e; e = e1[e]) {
+        int v = rep[dst[e]];
+        if (v != u) f[u] = max(f[u], dfs(v));
+    }
+    return f[u] += val[u];
+}
+
+int main() {
+    n = geti();
+    m = geti();
+    for (int i = 1; i <= n; ++i) val[i] = geti();
+    for (int i = 1; i <= m; ++i) {
+        int u = geti(), v = geti();
+        e1[i] = e0[u];
+        e0[u] = i;
+        if (!ee[u]) ee[u] = i;
+        dst[i] = v;
+    }
+    for (int i = 1; i <= n; ++i) tarjan(i);  // IMPORTANT: the graph has NO root
+    for (int i = 1; i <= m; ++i) dst[i] = rep[dst[i]];
+    for (int i = 1; i <= n; ++i) ans = max(ans, dfs(i));
+    putln(ans);
+    return 0;
+}
