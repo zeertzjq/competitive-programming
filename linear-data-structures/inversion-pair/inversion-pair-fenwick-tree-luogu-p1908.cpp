08090a06669c3b30f01e2687b6d9b7e3208a80eb
qjzTreeZ <35768171+qjzTreeZ@users.noreply.github.com>
2018-10-31 16:04:07 +0800

    Add inversion pair

---
+++ linear-data-structures/inversion-pair/inversion-pair-fenwick-tree-luogu-p1908.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+inline int geti() {
+    int x, f = 0;
+    char c;
+    while (!isdigit(c = getchar()))
+        if (c == '-') f = 1;
+    for (x = c - '0'; isdigit(c = getchar()); x = x * 10 + c - '0')
+        ;
+    return f ? -x : x;
+}
+
+template <typename T>
+void puti(T x) {
+    if (x < 0) {
+        putchar('-');
+        x = -x;
+    }
+    if (x > 9) puti(x / 10);
+    putchar(x % 10 + '0');
+}
+
+template <typename T>
+void putsp(T x) {
+    puti(x);
+    putchar(' ');
+}
+
+template <typename T>
+void putln(T x) {
+    puti(x);
+    putchar('\n');
+}
+
+const int N = 500010;
+int n, a[N], b[N], bit[N];
+long long ans = 0;
+
+inline void add(int k) {
+    while (k <= n) {
+        ++bit[k];
+        k += k & -k;
+    }
+}
+
+inline int qry(int k) {
+    int ret = 0;
+    while (k) {
+        ret += bit[k];
+        k &= k - 1;
+    }
+    return ret;
+}
+
+int main() {
+    n = geti();
+    for (int i = 1; i <= n; ++i) a[i] = b[i] = geti();
+    sort(b + 1, b + 1 + n);
+    for (int i = 1; i <= n; ++i) a[i] = lower_bound(b + 1, b + 1 + n, a[i]) - b;
+    for (int i = 1; i <= n; ++i) {
+        ans += i - 1 - qry(a[i]);
+        add(a[i]);
+    }
+    putln(ans);
+    return 0;
+}
---
+++ linear-data-structures/inversion-pair/inversion-pair-merge-sort-luogu-p1908.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+inline int geti() {
+    int x, f = 0;
+    char c;
+    while (!isdigit(c = getchar()))
+        if (c == '-') f = 1;
+    for (x = c - '0'; isdigit(c = getchar()); x = x * 10 + c - '0')
+        ;
+    return f ? -x : x;
+}
+
+template <typename T>
+void puti(T x) {
+    if (x < 0) {
+        putchar('-');
+        x = -x;
+    }
+    if (x > 9) puti(x / 10);
+    putchar(x % 10 + '0');
+}
+
+template <typename T>
+void putsp(T x) {
+    puti(x);
+    putchar(' ');
+}
+
+template <typename T>
+void putln(T x) {
+    puti(x);
+    putchar('\n');
+}
+
+const int N = 500010;
+int n, a[N], tmp[N];
+long long ans = 0;
+
+void solve(int l, int r) {
+    if (l >= r) return;
+    int m = (l + r) >> 1;
+    solve(l, m);
+    solve(m + 1, r);
+    int p = l, q = m + 1, o = l;
+    while (p <= m && q <= r)
+        if (a[p] > a[q]) {
+            ans += m - p + 1;
+            tmp[o++] = a[q++];
+        } else
+            tmp[o++] = a[p++];
+    while (p <= m) tmp[o++] = a[p++];
+    while (q <= r) tmp[o++] = a[q++];
+    copy(tmp + l, tmp + r + 1, a + l);
+}
+
+int main() {
+    n = geti();
+    for (int i = 1; i <= n; ++i) a[i] = geti();
+    solve(1, n);
+    putln(ans);
+    return 0;
+}
