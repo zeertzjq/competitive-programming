4387ee9c94f094656819006befc3a754e704df7c
qjzTreeZ <35768171+qjzTreeZ@users.noreply.github.com>
2018-11-05 16:11:58 +0800

    Modify utils

---
+++ utils/init.bash
@@ -0,0 +1,27 @@
+c() {
+    g++ $@ -Wall -Wextra -g -o "${PWD##*/}" "${PWD##*/}.cpp"
+}
+
+u() {
+    cp "${PWD##*/}$1.in" "${PWD##*/}.in"
+}
+
+x() {
+    ./${PWD##*/} && less "${PWD##*/}.out"
+}
+
+i() {
+    vim "${PWD##*/}$1.in"
+}
+
+o() {
+    vim "${PWD##*/}$1.out"
+}
+
+a() {
+    vim "${PWD##*/}$1.ans"
+}
+
+d() {
+    diff "${PWD##*/}$1.ans" "${PWD##*/}.out"
+}
