f1aa39b3461bde97643c05e8b3c8715c290f11dd
qjzTreeZ <35768171+qjzTreeZ@users.noreply.github.com>
2018-11-06 15:14:58 +0800

    Add sum of all subarray XORs

---
+++ bitwise-algorithms/sum-of-all-subarray-xors-luogu-p3917.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+inline int geti() {
+    int x, f = 0;
+    char c;
+    while (!isdigit(c = getchar()))
+        if (c == '-') f = 1;
+    for (x = c - '0'; isdigit(c = getchar()); x = x * 10 + c - '0')
+        ;
+    return f ? -x : x;
+}
+
+template <typename T>
+void puti(T x) {
+    if (x < 0) {
+        putchar('-');
+        x = -x;
+    }
+    if (x > 9) puti(x / 10);
+    putchar(x % 10 + '0');
+}
+
+template <typename T>
+void putsp(T x) {
+    puti(x);
+    putchar(' ');
+}
+
+template <typename T>
+void putln(T x) {
+    puti(x);
+    putchar('\n');
+}
+
+const int N = 100010, K = 32;
+int n, dp[2][K];
+bool f = 0;
+
+int main() {
+    n = geti();
+    long long ans = 0;
+    for (int i = 1; i <= n; ++i) {
+        int a = geti();
+        f ^= 1;
+        for (int j = 0; j < K; ++j) ans += (long long)(dp[f][j] = a & 1 << j ? i - dp[!f][j] : dp[!f][j]) << j;
+    }
+    putln(ans);
+    return 0;
+}
