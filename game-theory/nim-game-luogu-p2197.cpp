7dab9d33801dc2c4a7ed7b09977269f6a06c9688
qjzTreeZ <35768171+qjzTreeZ@users.noreply.github.com>
2018-11-06 11:39:23 +0800

    Add nim game

---
+++ game-theory/nim-game-luogu-p2197.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+inline int geti() {
+    int x, f = 0;
+    char c;
+    while (!isdigit(c = getchar()))
+        if (c == '-') f = 1;
+    for (x = c - '0'; isdigit(c = getchar()); x = x * 10 + c - '0')
+        ;
+    return f ? -x : x;
+}
+
+template <typename T>
+void puti(T x) {
+    if (x < 0) {
+        putchar('-');
+        x = -x;
+    }
+    if (x > 9) puti(x / 10);
+    putchar(x % 10 + '0');
+}
+
+template <typename T>
+void putsp(T x) {
+    puti(x);
+    putchar(' ');
+}
+
+template <typename T>
+void putln(T x) {
+    puti(x);
+    putchar('\n');
+}
+
+int main() {
+    int _ = geti();
+    while (_--) {
+        int n = geti(), xs = 0;
+        while (n--) xs ^= geti();
+        puts(xs ? "Yes" : "No");
+    }
+    return 0;
+}
