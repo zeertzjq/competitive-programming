84f5ed6c8eae384ca41b617b8039262c8488542f
qjzTreeZ <35768171+qjzTreeZ@users.noreply.github.com>
2018-12-21 19:34:38 +0800

    Modify persistent segment tree

--- persistent-data-structures/persistent-segment-tree-luogu-p3834.cpp
+++ persistent-data-structures/persistent-segment-tree-luogu-p3834.cpp
@@ -48,48 +48,37 @@ void putln(T x) {
 
 const int N = 200010;
 
-struct node {
-    int val;
-    node *c[2];
+int n, m, a[N], a0[N], rg, lc[N * 20], rc[N * 20], v[N * 20], rt[N], idx = 0;
 
-    node(int v, node *c0, node *c1) {
-        val = v;
-        c[0] = c0;
-        c[1] = c1;
-        if (!v && c0 && c1) upd();
-    }
-
-    void upd() {
-        val = c[0]->val + c[1]->val;
-    }
-};
-
-int n, m, a[N], a0[N], rg;
-node *rt[N], *nil = new node(0, NULL, NULL);
-
-node *add(node *rt, int l, int r, int x) {
-    if (x < l || x > r) return rt;
-    if (l == r) return new node(rt->val + 1, nil, nil);
-    int m = (l + r) >> 1;
-    return new node(0, add(rt->c[0], l, m, x), add(rt->c[1], m + 1, r, x));
+inline int mk(int val, int l, int r) {
+    v[++idx] = ~val ? val : v[l] + v[r];
+    lc[idx] = l;
+    rc[idx] = r;
+    return idx;
 }
 
-int qry(node *rt1, node *rt2, int l, int r, int k) {
-    if (l == r) return l;
-    int cnt = rt2->c[0]->val - rt1->c[0]->val;
+int add(int rt, int l, int r, int x) {
+    if (x < l || x > r) return rt;
+    if (l == r) return mk(v[rt] + 1, 0, 0);
     int m = (l + r) >> 1;
-    return k <= cnt ? qry(rt1->c[0], rt2->c[0], l, m, k) : qry(rt1->c[1], rt2->c[1], m + 1, r, k - cnt);
+    return mk(-1, add(lc[rt], l, m, x), add(rc[rt], m + 1, r, x));
+}
+
+int qry(int rt1, int rt2, int l, int r, int k) {
+    if (l == r) return l;
+    int cnt = v[lc[rt2]] - v[lc[rt1]];
+    int m = (l + r) >> 1;
+    return k <= cnt ? qry(lc[rt1], lc[rt2], l, m, k) : qry(rc[rt1], rc[rt2], m + 1, r, k - cnt);
 }
 
 int main() {
-    nil->c[0] = nil->c[1] = nil;
     n = geti();
     m = geti();
     for (int i = 1; i <= n; ++i) a[i] = geti();
     copy(a + 1, a + 1 + n, a0 + 1);
     sort(a + 1, a + 1 + n);
     rg = unique(a + 1, a + 1 + n) - a - 1;
-    rt[0] = nil;
+    rt[0] = mk(0, 0, 0);
     for (int i = 1; i <= n; ++i) rt[i] = add(rt[i - 1], 1, rg, lower_bound(a + 1, a + 1 + rg, a0[i]) - a);
     while (m--) {
         int l = geti(), r = geti();
