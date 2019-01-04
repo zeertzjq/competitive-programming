ed3c4c4eb8bfbfa00b2c8d88759e00412c0941c9
qjzTreeZ <35768171+qjzTreeZ@users.noreply.github.com>
2019-01-04 22:11:11 +0800

    Update persistent fhq treap

--- persistent-data-structures/persistent-fhq-treap-luogu-p5055.cpp
+++ persistent-data-structures/persistent-fhq-treap-luogu-p5055.cpp
@@ -46,45 +46,67 @@ void putln(T x) {
 }
 //}}}
 
-const int N = 500010, INF = 2147483647;
-int seed = 19260817, n, rt[N], key[N * 38], c[N * 38][2], pri[N * 38], sz[N * 38], tot = 0;
+const int N = 200010;
+int seed = 19260817, n, rt[N], val[N * 90], c[N * 90][2], pri[N * 90], sz[N * 90], tot = 0;
+bool rev[N * 90];
+long long s[N * 90], lastans = 0;
 
 inline int ran() {
     return seed = (seed * 1103515245LL + 12345LL) % 2147483648LL;
 }
 
-inline int mk(int k) {
-    key[++tot] = k;
+inline int mk(int v) {
+    val[++tot] = v;
     pri[tot] = ran();
     sz[tot] = 1;
+    s[tot] = v;
     return tot;
 }
 
 inline int cp(int o) {
-    key[++tot] = key[o];
+    val[++tot] = val[o];
     pri[tot] = pri[o];
     c[tot][0] = c[o][0];
     c[tot][1] = c[o][1];
     sz[tot] = sz[o];
+    s[tot] = s[o];
+    rev[tot] = rev[o];
     return tot;
 }
 
 inline void upd(int p) {
     sz[p] = 1 + sz[c[p][0]] + sz[c[p][1]];
+    s[p] = val[p] + s[c[p][0]] + s[c[p][1]];
 }
 
-void split(int rt, int k, int &l, int &r) {
+inline void push(int p) {
+    if (!rev[p]) return;
+    swap(c[p][0], c[p][1]);
+    if (c[p][0]) {
+        c[p][0] = cp(c[p][0]);
+        rev[c[p][0]] ^= 1;
+    }
+    if (c[p][1]) {
+        c[p][1] = cp(c[p][1]);
+        rev[c[p][1]] ^= 1;
+    }
+    rev[p] = 0;
+}
+
+void split(int rt, int rk, int &l, int &r) {
     if (!rt) {
         l = r = 0;
         return;
     }
     rt = cp(rt);
-    if (key[rt] <= k) {
-        l = rt;
-        split(c[rt][1], k, c[rt][1], r);
-    } else {
+    push(rt);
+    int lsz = sz[c[rt][0]];
+    if (rk <= lsz) {
         r = rt;
-        split(c[rt][0], k, l, c[rt][0]);
+        split(c[rt][0], rk, l, c[rt][0]);
+    } else {
+        l = rt;
+        split(c[rt][1], rk - lsz - 1, c[rt][1], r);
     }
     upd(rt);
 }
@@ -92,69 +114,46 @@ void split(int rt, int k, int &l, int &r) {
 int merge(int l, int r) {
     if (!l || !r) return l | r;
     if (pri[l] > pri[r]) {
+        push(l);
         c[l][1] = merge(c[l][1], r);
         upd(l);
         return l;
     } else {
+        push(r);
         c[r][0] = merge(l, c[r][0]);
         upd(r);
         return r;
     }
 }
 
-inline int maxn(int rt) {
-    while (c[rt][1]) rt = c[rt][1];
-    return key[rt];
-}
-
-inline int minn(int rt) {
-    while (c[rt][0]) rt = c[rt][0];
-    return key[rt];
-}
-
-int kth(int rt, int rk) {
-    int lsz = sz[c[rt][0]];
-    if (rk <= lsz)
-        return kth(c[rt][0], rk);
-    else if (rk == lsz + 1)
-        return key[rt];
-    else
-        return kth(c[rt][1], rk - lsz - 1);
-}
-
 int main() {
     n = geti();
     for (int i = 1; i <= n; ++i) {
-        int v = geti(), o = geti(), x = geti();
+        int v = geti(), o = geti();
         if (o == 1) {
+            int p = geti() ^ lastans, x = geti() ^ lastans;
             int t1, t2;
-            split(rt[v], x, t1, t2);
+            split(rt[v], p, t1, t2);
             rt[i] = merge(merge(t1, mk(x)), t2);
         } else if (o == 2) {
+            int p = geti() ^ lastans;
             int t1, t2, t3;
-            split(rt[v], x, t1, t3);
-            split(t1, x - 1, t1, t2);
-            if (t2)
-                rt[i] = merge(merge(t1, merge(c[t2][0], c[t2][1])), t3);
-            else
-                rt[i] = rt[v];
+            split(rt[v], p, t1, t3);
+            split(t1, p - 1, t1, t2);
+            rt[i] = merge(t1, t3);
         } else if (o == 3) {
-            int t1, t2;
-            split(rt[v], x - 1, t1, t2);
-            putln(sz[t1] + 1);
-            rt[i] = rt[v];
+            int l = geti() ^ lastans, r = geti() ^ lastans;
+            int t1, t2, t3;
+            split(rt[v], r, t1, t3);
+            split(t1, l - 1, t1, t2);
+            rev[t2] ^= 1;
+            rt[i] = merge(merge(t1, t2), t3);
         } else if (o == 4) {
-            putln(kth(rt[v], x));
-            rt[i] = rt[v];
-        } else if (o == 5) {
-            int t1, t2;
-            split(rt[v], x - 1, t1, t2);
-            putln(t1 ? maxn(t1) : -INF);
-            rt[i] = rt[v];
-        } else {
-            int t1, t2;
-            split(rt[v], x, t1, t2);
-            putln(t2 ? minn(t2) : INF);
+            int l = geti() ^ lastans, r = geti() ^ lastans;
+            int t1, t2, t3;
+            split(rt[v], r, t1, t3);
+            split(t1, l - 1, t1, t2);
+            putln(lastans = s[t2]);
             rt[i] = rt[v];
         }
     }
