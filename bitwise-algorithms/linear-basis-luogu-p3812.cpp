8f4c71cf67ca62f58a0c1eaff6b513a94dc2d082
qjzTreeZ <qjztreez@gmail.com>
2019-03-15 21:36:28 +0800

    Small changes

--- bitwise-algorithms/linear-basis-luogu-p3812.cpp
+++ bitwise-algorithms/linear-basis-luogu-p3812.cpp
@@ -5,8 +5,7 @@ using namespace std;
 inline int gi() {
     int x, f = 0;
     char c;
-    while (!isdigit(c = getchar()))
-        if (c == '-') f = 1;
+    while (!isdigit(c = getchar())) c == '-' && (f = 1);
     for (x = c - '0'; isdigit(c = getchar()); x = x * 10 + c - '0')
         ;
     return f ? -x : x;
@@ -16,8 +15,7 @@ inline long long gll() {
     int f = 0;
     long long x;
     char c;
-    while (!isdigit(c = getchar()))
-        if (c == '-') f = 1;
+    while (!isdigit(c = getchar())) c == '-' && (f = 1);
     for (x = c - '0'; isdigit(c = getchar()); x = x * 10 + c - '0')
         ;
     return f ? -x : x;
@@ -25,24 +23,19 @@ inline long long gll() {
 
 template <typename T>
 void puti(T x) {
-    if (x < 0) {
-        putchar('-');
-        x = -x;
-    }
+    if (x < 0) putchar('-'), x = -x;
     if (x > 9) puti(x / 10);
     putchar(x % 10 + '0');
 }
 
 template <typename T>
 inline void putsp(T x) {
-    puti(x);
-    putchar(' ');
+    puti(x), putchar(' ');
 }
 
 template <typename T>
 inline void putln(T x) {
-    puti(x);
-    putchar('\n');
+    puti(x), putchar('\n');
 }
 //}}}
 
