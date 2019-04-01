be93de3a24baaec040e3d35cfeb6cf7ce0d31455
qjzTreeZ <qjztreez@gmail.com>
2019-04-01 20:51:36 +0800

    Add area of polygon

---
+++ mathematical-algorithms/computational-geometry/area-of-polygon/luogu-p1183.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//{{{
+inline int gi() {
+    int x, f = 0;
+    char c;
+    while (!isdigit(c = getchar())) c == '-' && (f = 1);
+    for (x = c - '0'; isdigit(c = getchar()); x = x * 10 + c - '0')
+        ;
+    return f ? -x : x;
+}
+
+inline long long gll() {
+    int f = 0;
+    long long x;
+    char c;
+    while (!isdigit(c = getchar())) c == '-' && (f = 1);
+    for (x = c - '0'; isdigit(c = getchar()); x = x * 10 + c - '0')
+        ;
+    return f ? -x : x;
+}
+
+template <typename T>
+void puti(T x) {
+    if (x < 0) putchar('-'), x = -x;
+    if (x > 9) puti(x / 10);
+    putchar(x % 10 + '0');
+}
+
+template <typename T>
+inline void putsp(T x) {
+    puti(x), putchar(' ');
+}
+
+template <typename T>
+inline void putln(T x) {
+    puti(x), putchar('\n');
+}
+//}}}
+
+int main() {
+    int n = gi(), x0 = gi(), y0 = gi(), xx = x0, yy = y0, ans2 = 0;
+    for (int i = 1; i < n; ++i) {
+        int x = gi(), y = gi();
+        ans2 += xx * y - yy * x, xx = x, yy = y;
+    }
+    ans2 += xx * y0 - yy * x0, putln(ans2 >> 1);
+    return 0;
+}
